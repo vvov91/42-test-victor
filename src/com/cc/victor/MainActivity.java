cdca2231d44442725dff9c5f838f9d7aae43dfe9
victor <vitek91@gmail.com>
2013-09-10 10:36:29 +0300

    ticket:1 Setting up project and ActionBarSherlock

---
+++ src/com/cc/victor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cc.victor;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	}
+
+}
