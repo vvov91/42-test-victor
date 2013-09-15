379dfbd609fb7f9bd88640c08b938850f5c2f62d
Victor <v.vovchenko91@gmail.com>
2013-09-15 19:43:43 +0300

    ticket:3 Fix datepicker when nothing selected

--- src/com/cc/victor/DatePickerActivity.java
+++ src/com/cc/victor/DatePickerActivity.java
@@ -16,9 +16,17 @@ import android.widget.DatePicker;
 import android.widget.Toast;
 import android.widget.DatePicker.OnDateChangedListener;
 
+/**
+ * Date picking dialog activity
+ * 
+ * @author Victor Vovchenko <vitek91@gmail.com>
+ * 
+ */
 public class DatePickerActivity extends Activity {
 	
 	private String mDate = ""; 
+	
+	private final SimpleDateFormat mSdf = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,12 @@ public class DatePickerActivity extends Activity {
 		requestWindowFeature((int) Window.FEATURE_NO_TITLE);	
 		setContentView(R.layout.activity_datepicker);			
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
-		sdf.setTimeZone(TimeZone.getDefault());
+		mSdf.setTimeZone(TimeZone.getDefault());
 		
 		Calendar today = Calendar.getInstance();
 		try {
-			today.setTimeInMillis(sdf.parse(getIntent().getStringExtra("date")).getTime());
+			// parsing date from user data
+			today.setTimeInMillis(mSdf.parse(getIntent().getStringExtra("date")).getTime());
 		} catch (ParseException e) { }
 		
 		DatePicker datepicker = (DatePicker) findViewById(R.id.datepicker);
@@ -41,26 +49,31 @@ public class DatePickerActivity extends Activity {
 					@Override
 					public void onDateChanged(DatePicker view, int year, int monthOfYear,
 							int dayOfMonth) {
-						mDate = new StringBuilder().append(dayOfMonth).append(" ")
-								.append(monthOfYear + 1).append(" ").append(year).toString();
+						mDate = new StringBuilder().append(dayOfMonth).append("-")
+								.append(monthOfYear + 1).append("-").append(year).toString();
 					}
 					
 				});
 	}
 	
-	public void okButtonClick(View v) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd MM yyyy", Locale.US);
-		sdf.setTimeZone(TimeZone.getDefault());
-		
+	public void okButtonClick(View v) {		
 		long date = 0;
 		try {
-			date = sdf.parse(mDate).getTime();
+			// if nothing changed
+			if (mDate.equals("")) {
+				// set date from user data
+				mDate = getIntent().getStringExtra("date");
+			}
+			
+			date = mSdf.parse(mDate).getTime();
 		} catch (ParseException e) { }
 		
+		// if selected date is more than now
 		if (date > System.currentTimeMillis()) {
 			Toast.makeText(getApplicationContext(),
 					R.string.dateofbitrh_error, Toast.LENGTH_LONG).show();
-			
+
+			// do nothing
 			return;
 		}		
 		
